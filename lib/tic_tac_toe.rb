8eb9f901f23ef915bc6b4e5a26c0634544adfe12
BEAU WITKA <bwitka@gmail.com>
2019-04-03 01:53:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,6 +23,13 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
+  def input_to_index(user_input)
+    converted_input = user_input.to_i - 1
+    return @board[converted_input]
+  end
+  
+  
+  
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
@@ -36,26 +43,9 @@ end
 
 
 
-
-
-
-
-
-
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def input_to_index(user_input)
-  converted_input = user_input.to_i - 1
-end
+
 
 def move(array, index, value)
   array[index] = value
