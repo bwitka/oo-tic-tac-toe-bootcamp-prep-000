b6b6d38c435971d944d9ef5adc42d8e43c799dea
BEAU WITKA <bwitka@gmail.com>
2019-04-03 02:27:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,7 +69,7 @@ class TicTacToe
   
   def turn_count(board)
     counter = 0 
-    @board.each do |element|
+    board.each do |element|
       if element == "X" || element == "O"
         counter += 1 
       end
@@ -78,7 +78,7 @@ class TicTacToe
   end
 
   def current_player(board)
-    turn_count(@board).even? ? "X" : "O"
+    turn_count(board).even? ? "X" : "O"
   end
   
   def won?(board) 
