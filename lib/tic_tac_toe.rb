7b26154932c259b82ec93af9ff25085ca2a9b0ab
BEAU WITKA <bwitka@gmail.com>
2019-04-03 02:22:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ class TicTacToe
   end
 
   def position_taken?(board, index_number)
-    if board[index_number] == " " || board[index_number] == "" || board[index_number] == nil
+    if @board[index_number] == " " || @board[index_number] == "" || @board[index_number] == nil
       false
     else
       true
