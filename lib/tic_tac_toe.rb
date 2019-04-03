d0ca3a6197dd351271635e597570735ebcb29c5e
BEAU WITKA <bwitka@gmail.com>
2019-04-03 02:28:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,27 +59,27 @@ class TicTacToe
     end
   end
  
-  # def turn_count
-  #   @board.count{|token| token == "X" || token == "O"}
-  # end
-  
-  # def current_player
-  #   turn_count % 2 == 0 ? "X" : "O"
-  # end
-  
-  def turn_count(board)
-    counter = 0 
-    board.each do |element|
-      if element == "X" || element == "O"
-        counter += 1 
-      end
-    end
-    return counter
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
   end
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  # def turn_count(board)
+  #   counter = 0 
+  #   board.each do |element|
+  #     if element == "X" || element == "O"
+  #       counter += 1 
+  #     end
+  #   end
+  #   return counter
+  # end
 
-  def current_player(board)
-    turn_count(board).even? ? "X" : "O"
-  end
+  # def current_player(board)
+  #   turn_count(board).even? ? "X" : "O"
+  # end
   
   def won?(board) 
     WIN_COMBINATIONS.each do |win_combination| 
