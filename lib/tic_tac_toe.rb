8d75f12a6ad70c4afe7f955985eddc6f9eda49ff
BEAU WITKA <bwitka@gmail.com>
2019-04-03 01:50:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,14 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
@@ -23,13 +31,7 @@ class TicTacToe
     @board.count{|token| token == "X" || token == "O"}
   end
  
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
+  
 end
 
 
